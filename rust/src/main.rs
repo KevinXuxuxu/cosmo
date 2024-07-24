a9d85805617c7240b231311edf997f809c9c50f0
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2024-07-23 21:51:27 -0700

    add perf stats

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::time::Instant;
 
 const cursor_up: &str = "\x1B[F";
 const clear_line: &str = "\x1B[K";
@@ -10,6 +11,8 @@ struct Player {
     h: usize,
     a: Vec<Vec<char>>,
     t: f32,
+    dt: f32,
+    dt_millis: u64
 }
 
 fn udiff(a: usize, b: usize) -> usize {
@@ -21,11 +24,13 @@ impl Player {
         let empty_str = vec![' '; w];
         let a = vec![empty_str; h];
         let t = 0.0;
-        Player { fr, w, h, a, t }
+        let dt = 1.0 / (fr as f32);
+        let dt_millis = (dt * 1000.0) as u64;
+        Player { fr, w, h, a, t, dt, dt_millis}
     }
 
     pub fn render(&self) {
-        println!("{}", cursor_up.repeat(self.h));
+        println!("{}", cursor_up.repeat(self.h + 1));
         for l in &self.a {
             let l_str: String = l.into_iter().collect();
             println!("{}{}", clear_line, l_str);
@@ -44,20 +49,25 @@ impl Player {
                 }
             }
         }
-        let dt = 1.0 / (self.fr as f32);
-        let dt_millis = (dt * 1000.0) as u64;
-        thread::sleep(Duration::from_millis(dt_millis));
-        self.t += dt;
+    }
+
+    pub fn run(&mut self, duration: f32) {
+        loop {
+            let start = Instant::now();
+            self.update();
+            self.render();
+            let compute_t_millis = start.elapsed().as_millis() as u64;
+            self.t += self.dt;
+            println!("{}compute_time: {} wait_time: {}", clear_line, compute_t_millis, self.dt_millis - compute_t_millis);
+            if self.t > duration {
+                break;
+            }
+            thread::sleep(Duration::from_millis(self.dt_millis - compute_t_millis));
+        }
     }
 }
 
 fn main() {
     let mut p = Player::new(24, 30, 30);
-    loop {
-        p.update();
-        p.render();
-        if p.t > 20.0 {
-            break;
-        }
-    }
+    p.run(20.);
 }
